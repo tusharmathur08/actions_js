a1995f260a01e04d15a3c24f5bbf576db3a5297f
Tushar Mathur <37363902+tusharmathur08@users.noreply.github.com>
2023-11-29 19:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-(
-    async () => {
-        let x=5
-        let y=6
-        z = x + y;
-        z
-        try {
-         core.notice("calling action");
-        } catch (error) {
-            core.setFailed(error.message);
-        }
-    }
-)();
+let x = 5;
+let y = 6;
+z = x + y;
+console.log(z);
